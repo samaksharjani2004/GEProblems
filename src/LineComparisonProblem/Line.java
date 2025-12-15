7d87aa19de26b604cefb83e4f783783257d979d2
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 14:12:17 +0530

    LC UC1: Check equality of two lines using equals method

--- src/LineComparisonProblem/Line.java
+++ src/LineComparisonProblem/Line.java
@@ -3,9 +3,24 @@ package LineComparisonProblem;
 public class Line {
     Point start;
     Point end;
+    Double length;
 
     public Line(Point start, Point end) {
         this.start = start;
         this.end = end;
+        this.length = calculateLength();
+    }
+
+    private double calculateLength() {
+        return Math.sqrt(
+                Math.pow(end.x - start.x, 2) +
+                        Math.pow(end.y - start.y, 2)
+        );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Line other = (Line) obj;
+        return this.length.equals(other.length);
     }
 }
--- src/LineComparisonProblem/Main.java
+++ src/LineComparisonProblem/Main.java
@@ -2,6 +2,21 @@ package LineComparisonProblem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+
+        Line line1 = new Line(
+                new Point(0, 0),
+                new Point(4, 4)
+        );
+
+        Line line2 = new Line(
+                new Point(0, 0),
+                new Point(4, 4)
+        );
+
+        if (line1.equals(line2)) {
+            System.out.println("Both lines are equal");
+        } else {
+            System.out.println("Lines are not equal");
+        }
     }
 }
