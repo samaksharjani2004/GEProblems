58949961ee82f5e4761d21ece7b918159c458da0
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 14:08:03 +0530

    Initial commit: Welcome to Line Comparison Computation Program

---
+++ src/LineComparisonProblem/Line.java
@@ -0,0 +1,11 @@
+package LineComparisonProblem;
+
+public class Line {
+    Point start;
+    Point end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+}
--- src/LineComparisonProblem/Main.java
+++ src/LineComparisonProblem/Main.java
@@ -1,4 +1,7 @@
 package LineComparisonProblem;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
 }
