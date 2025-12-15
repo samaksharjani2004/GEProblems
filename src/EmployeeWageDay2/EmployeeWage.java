79785d5a4c12af5ed77b1cb97f33ce0f2f487608
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 15:11:05 +0530

    UC2: Add part time employee and wage calculation

--- src/EmployeeWageDay2/EmployeeWage.java
+++ src/EmployeeWageDay2/EmployeeWage.java
@@ -1,4 +1,30 @@
 package EmployeeWageDay2;
 
 public class EmployeeWage {
+
+    public void calculateDailyWage() {
+
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+
+        int wagePerHour = 20;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
+
+        int empCheck = (int) (Math.random() * 3);
+        int empHours = 0;
+
+        if (empCheck == IS_FULL_TIME) {
+            empHours = fullTimeHour;
+            System.out.println("Employee is Full Time");
+        } else if (empCheck == IS_PART_TIME) {
+            empHours = partTimeHour;
+            System.out.println("Employee is Part Time");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+        int dailyWage = empHours * wagePerHour;
+        System.out.println("Daily Wage: " + dailyWage);
+    }
 }
--- src/EmployeeWageDay2/Main.java
+++ src/EmployeeWageDay2/Main.java
@@ -2,6 +2,10 @@ package EmployeeWageDay2;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.calculateDailyWage();
     }
 }
