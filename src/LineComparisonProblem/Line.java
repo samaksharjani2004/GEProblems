10179ed61f0191d9bc580fdd2cc3c72363d2d7a6
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 14:14:51 +0530

    LC UC2: Compare two lines using compareTo method

--- src/LineComparisonProblem/Line.java
+++ src/LineComparisonProblem/Line.java
@@ -1,11 +1,27 @@
 package LineComparisonProblem;
 
-public class Line {
+public class Line implements Comparable<Line> {
+
     Point start;
     Point end;
+    Double length;
 
     public Line(Point start, Point end) {
         this.start = start;
         this.end = end;
+        this.length = calculateLength();
+    }
+
+    private double calculateLength() {
+        return Math.sqrt(
+                Math.pow(end.x - start.x, 2) +
+                        Math.pow(end.y - start.y, 2)
+        );
+    }
+
+    @Override
+    public int compareTo(Line other) {
+        return this.length.compareTo(other.length);
     }
 }
+
--- src/LineComparisonProblem/Main.java
+++ src/LineComparisonProblem/Main.java
@@ -2,6 +2,25 @@ package LineComparisonProblem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+
+        Line line1 = new Line(
+                new Point(1, 1),
+                new Point(5, 5)
+        );
+
+        Line line2 = new Line(
+                new Point(2, 2),
+                new Point(6, 6)
+        );
+
+        int result = line1.compareTo(line2);
+
+        if (result == 0) {
+            System.out.println("Both lines are equal");
+        } else if (result > 0) {
+            System.out.println("Line 1 is greater than Line 2");
+        } else {
+            System.out.println("Line 1 is smaller than Line 2");
+        }
     }
 }
