4e825c2e3c39bbdeddf24b79a18d2f6689d7fd21
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 17:00:23 +0530

    SL-UC4: Move position based on option

--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -2,4 +2,32 @@ package SnakeAndLadder;
 
 public class SnakeLadder {
     int position = 0;
+
+    public int rollDie() {
+        return (int)(Math.random() * 6) + 1;
+    }
+
+    public int checkOption() {
+        return (int)(Math.random() * 3);
+    }
+
+    public void playTurn() {
+        int dice = rollDie();
+        int option = checkOption();
+
+        switch (option) {
+            case 1: // Ladder
+                position += dice;
+                break;
+            case 2: // Snake
+                position -= dice;
+                if (position < 0)
+                    position = 0;
+                break;
+            default:
+                break;
+        }
+
+        System.out.println("Position: " + position);
+    }
 }
