1678baa579fb708b224ee248374e3730fcf37c02
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 15:08:19 +0530

    EW UC1: Employee attendance

--- src/EmployeeWageDay2/EmployeeWage.java
+++ src/EmployeeWageDay2/EmployeeWage.java
@@ -1,4 +1,23 @@
 package EmployeeWageDay2;
 
 public class EmployeeWage {
+
+    public void calculateDailyWage() {
+
+        int IS_PRESENT = 1;
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
+        int empCheck = (int) (Math.random() * 2);
+
+        if (empCheck == IS_PRESENT) {
+            int dailyWage = wagePerHour * fullDayHour;
+            System.out.println("Employee is Present");
+            System.out.println("Daily Wage: " + dailyWage);
+        } else {
+            System.out.println("Employee is Absent");
+            System.out.println("Daily Wage: 0");
+        }
+    }
 }
+
--- src/EmployeeWageDay2/Main.java
+++ src/EmployeeWageDay2/Main.java
@@ -1,7 +1,12 @@
 package EmployeeWageDay2;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.calculateDailyWage();
     }
 }
