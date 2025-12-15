0c57ce35d5d32db6d74c2e705d8236ef6664ee56
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 17:08:42 +0530

    SL-UC7: Two player game with ladder extra chance and winner

--- src/SnakeAndLadder/Main.java
+++ src/SnakeAndLadder/Main.java
@@ -1,8 +1,42 @@
 package SnakeAndLadder;
 
 public class Main {
+
     public static void main(String[] args) {
-        SnakeLadder player = new SnakeLadder();
-        System.out.println("Player starting position: " + player.position);
+
+        SnakeLadder player1 = new SnakeLadder();
+        SnakeLadder player2 = new SnakeLadder();
+
+        boolean playerOneTurn = true;
+
+        System.out.println("Two Player Snake & Ladder Game Started");
+
+        while (true) {
+
+            if (playerOneTurn) {
+                boolean extraTurn = player1.playTurn("Player 1");
+
+                if (player1.position == 100) {
+                    System.out.println("\n🎉 Player 1 WON!");
+                    System.out.println("Dice Rolls: " + player1.diceCount);
+                    break;
+                }
+
+                if (!extraTurn)
+                    playerOneTurn = false;
+
+            } else {
+                boolean extraTurn = player2.playTurn("Player 2");
+
+                if (player2.position == 100) {
+                    System.out.println("\n🎉 Player 2 WON!");
+                    System.out.println("Dice Rolls: " + player2.diceCount);
+                    break;
+                }
+
+                if (!extraTurn)
+                    playerOneTurn = true;
+            }
+        }
     }
 }
--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -1,5 +1,51 @@
 package SnakeAndLadder;
 
 public class SnakeLadder {
+
     int position = 0;
+    int diceCount = 0;
+
+    public int rollDie() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    public int checkOption() {
+        return (int) (Math.random() * 3);
+    }
+
+    // returns true if Ladder (extra turn)
+    public boolean playTurn(String playerName) {
+
+        int dice = rollDie();
+        diceCount++;
+
+        int option = checkOption();
+        int previousPosition = position;
+
+        switch (option) {
+            case 1: // Ladder
+                position += dice;
+                System.out.println(playerName + " got Ladder");
+                break;
+
+            case 2: // Snake
+                position -= dice;
+                if (position < 0)
+                    position = 0;
+                System.out.println(playerName + " got Snake");
+                break;
+
+            default:
+                System.out.println(playerName + " got No Play");
+        }
+
+        if (position > 100) {
+            position = previousPosition;
+        }
+
+        System.out.println(playerName + " rolled " + dice +
+                " | Position: " + position);
+
+        return option == 1;
+    }
 }
