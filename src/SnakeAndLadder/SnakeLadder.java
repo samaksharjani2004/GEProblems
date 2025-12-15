f6bc9a87ec6a0a2edb61b5135e1a09945a12da2c
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 17:03:07 +0530

    SL-UC5: Exact winning position 100

--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -2,4 +2,35 @@ package SnakeAndLadder;
 
 public class SnakeLadder {
     int position = 0;
+
+    public int rollDie() {
+        return (int)(Math.random() * 6) + 1;
+    }
+
+    public int checkOption() {
+        return (int)(Math.random() * 3);
+    }
+
+    public void playTurn() {
+        int dice = rollDie();
+        int option = checkOption();
+        int previousPosition = position;
+
+        switch (option) {
+            case 1:
+                position += dice;
+                break;
+            case 2:
+                position -= dice;
+                if (position < 0)
+                    position = 0;
+                break;
+        }
+
+        if (position > 100) {
+            position = previousPosition;
+        }
+
+        System.out.println("Position: " + position);
+    }
 }
