a877b7f2082013e9c92d0dcdd07ee62098faa433
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 16:57:17 +0530

    SL-UC2: Roll a die (1-6)

--- src/SnakeAndLadder/Main.java
+++ src/SnakeAndLadder/Main.java
@@ -3,6 +3,7 @@ package SnakeAndLadder;
 public class Main {
     public static void main(String[] args) {
         SnakeLadder player = new SnakeLadder();
-        System.out.println("Player starting position: " + player.position);
+        int dice = player.rollDie();
+        System.out.println("Dice rolled: " + dice);
     }
 }
--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -2,4 +2,8 @@ package SnakeAndLadder;
 
 public class SnakeLadder {
     int position = 0;
+
+    public int rollDie() {
+        return (int)(Math.random() * 6) + 1;
+    }
 }
