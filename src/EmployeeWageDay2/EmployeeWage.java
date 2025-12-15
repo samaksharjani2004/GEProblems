e1b07b8ec5282e16a18d0ae40bf879c12131a3a2
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 15:23:54 +0530

    UC4: Calculate monthly employee wage

--- src/EmployeeWageDay2/EmployeeWage.java
+++ src/EmployeeWageDay2/EmployeeWage.java
@@ -1,4 +1,14 @@
 package EmployeeWageDay2;
 
 public class EmployeeWage {
+
+    public void calculateMonthlyWage() {
+
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int workingDays = 20;
+
+        int monthlyWage = wagePerHour * fullDayHour * workingDays;
+        System.out.println("Monthly Wage: " + monthlyWage);
+    }
 }
