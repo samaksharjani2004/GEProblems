0bac43c30b039d537dab37e2212daf7249954128
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 16:58:47 +0530

    SL-UC3: Check option No Play, Ladder, Snake

--- src/SnakeAndLadder/Main.java
+++ src/SnakeAndLadder/Main.java
@@ -3,6 +3,7 @@ package SnakeAndLadder;
 public class Main {
     public static void main(String[] args) {
         SnakeLadder player = new SnakeLadder();
-        System.out.println("Player starting position: " + player.position);
+        int option = player.checkOption();
+        System.out.println("Option: " + option);
     }
 }
--- src/SnakeAndLadder/SnakeLadder.java
+++ src/SnakeAndLadder/SnakeLadder.java
@@ -2,4 +2,12 @@ package SnakeAndLadder;
 
 public class SnakeLadder {
     int position = 0;
+
+    public int rollDie() {
+        return (int)(Math.random() * 6) + 1;
+    }
+
+    public int checkOption() {
+        return (int)(Math.random() * 3); // 0-No Play, 1-Ladder, 2-Snake
+    }
 }
