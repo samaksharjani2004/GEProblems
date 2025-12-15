2653d3eec7d1da6f192894f2031951840ff8f77b
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-15 15:26:58 +0530

    UC5: Calculate wage till max hours or days reached

--- src/EmployeeWageDay2/EmployeeWage.java
+++ src/EmployeeWageDay2/EmployeeWage.java
@@ -1,4 +1,37 @@
 package EmployeeWageDay2;
 
 public class EmployeeWage {
+
+    public void calculateWageTillCondition() {
+
+        int wagePerHour = 20;
+        int maxWorkingDays = 20;
+        int maxWorkingHours = 100;
+
+        int totalWorkingDays = 0;
+        int totalWorkingHours = 0;
+
+        while (totalWorkingDays < maxWorkingDays &&
+                totalWorkingHours < maxWorkingHours) {
+
+            totalWorkingDays++;
+
+            int empCheck = (int) (Math.random() * 3);
+            int empHours = 0;
+
+            if (empCheck == 1) {
+                empHours = 8;
+            } else if (empCheck == 2) {
+                empHours = 4;
+            }
+
+            totalWorkingHours += empHours;
+        }
+
+        int totalWage = totalWorkingHours * wagePerHour;
+
+        System.out.println("Total Working Days: " + totalWorkingDays);
+        System.out.println("Total Working Hours: " + totalWorkingHours);
+        System.out.println("Total Wage: " + totalWage);
+    }
 }
