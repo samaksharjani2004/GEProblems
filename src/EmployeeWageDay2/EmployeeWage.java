6a811b830dc85d645ce5aec1bf90484c112f24d2
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-16 13:11:25 +0530

    UC8: Compute employee wage using class method with parameters

--- src/EmployeeWageDay2/EmployeeWage.java
+++ src/EmployeeWageDay2/EmployeeWage.java
@@ -1,4 +1,49 @@
 package EmployeeWageDay2;
 
 public class EmployeeWage {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+
+    // UC-8: Class Method with Parameters
+    public static void computeEmployeeWage(String company,
+                                           int empRatePerHour,
+                                           int maxWorkingDays,
+                                           int maxWorkingHours) {
+
+        int empHours;
+        int totalEmpHours = 0;
+        int totalWorkingDays = 0;
+
+        while (totalEmpHours < maxWorkingHours &&
+                totalWorkingDays < maxWorkingDays) {
+
+            totalWorkingDays++;
+
+            int empCheck = (int) (Math.random() * 10) % 3;
+
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+
+                default:
+                    empHours = 0;
+            }
+
+            totalEmpHours += empHours;
+        }
+
+        int totalEmpWage = totalEmpHours * empRatePerHour;
+
+        System.out.println("Company: " + company);
+        System.out.println("Total Working Days: " + totalWorkingDays);
+        System.out.println("Total Working Hours: " + totalEmpHours);
+        System.out.println("Total Employee Wage: " + totalEmpWage);
+        System.out.println("------------------------------------");
+    }
 }
--- src/EmployeeWageDay2/Main.java
+++ src/EmployeeWageDay2/Main.java
@@ -2,6 +2,12 @@ package EmployeeWageDay2;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        // UC-8: Passing company details as parameters
+        EmployeeWage.computeEmployeeWage("DMart", 20, 20, 100);
+        EmployeeWage.computeEmployeeWage("Reliance", 25, 22, 110);
+        EmployeeWage.computeEmployeeWage("TCS", 30, 20, 120);
     }
 }
