c4d0c178a4e705ee5db85e35d23c8740413f84e6
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-16 13:00:48 +0530

    UC6: Compute employee wage for multiple companies

--- src/EmployeeWageDay2/EmployeeWage.java
+++ src/EmployeeWageDay2/EmployeeWage.java
@@ -1,4 +1,58 @@
 package EmployeeWageDay2;
 
 public class EmployeeWage {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+
+    private String company;
+    private int empRatePerHour;
+    private int maxWorkingDays;
+    private int maxWorkingHours;
+
+    public EmployeeWage(String company, int empRatePerHour,
+                        int maxWorkingDays, int maxWorkingHours) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.maxWorkingDays = maxWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+    }
+
+    public void computeEmployeeWage() {
+
+        int empHours;
+        int totalEmpHours = 0;
+        int totalWorkingDays = 0;
+
+        while (totalEmpHours < maxWorkingHours &&
+                totalWorkingDays < maxWorkingDays) {
+
+            totalWorkingDays++;
+
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+
+                default:
+                    empHours = 0;
+            }
+
+            totalEmpHours += empHours;
+        }
+
+        int totalEmpWage = totalEmpHours * empRatePerHour;
+
+        System.out.println("Company: " + company);
+        System.out.println("Total Working Days: " + totalWorkingDays);
+        System.out.println("Total Working Hours: " + totalEmpHours);
+        System.out.println("Total Employee Wage: " + totalEmpWage);
+        System.out.println("-----------------------------------");
+    }
 }
--- src/EmployeeWageDay2/Main.java
+++ src/EmployeeWageDay2/Main.java
@@ -2,6 +2,20 @@ package EmployeeWageDay2;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        EmployeeWage dMart =
+                new EmployeeWage("DMart", 20, 20, 100);
+
+        EmployeeWage reliance =
+                new EmployeeWage("Reliance", 25, 22, 110);
+
+        EmployeeWage tcs =
+                new EmployeeWage("TCS", 30, 20, 120);
+
+        dMart.computeEmployeeWage();
+        reliance.computeEmployeeWage();
+        tcs.computeEmployeeWage();
     }
 }
