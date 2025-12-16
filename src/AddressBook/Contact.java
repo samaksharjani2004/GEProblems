6a6555f2d67daf81b7fc3ba2f5a39c319a2b07c0
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-16 13:51:51 +0530

    Udated Address Book main

--- src/AddressBook/Contact.java
+++ src/AddressBook/Contact.java
@@ -1,4 +1,33 @@
 package AddressBook;
 
 public class Contact {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phoneNumber, String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public void display() {
+        System.out.println(firstName + " " + lastName + " | " +
+                address + ", " + city + ", " + state + " | " +
+                zip + " | " + phoneNumber + " | " + email);
+    }
 }
