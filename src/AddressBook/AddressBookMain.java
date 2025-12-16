a8bb736e60aaa2c238157809a1c6b79ea998b6bf
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-16 13:54:57 +0530

    AB-UC2: Edit existing contact using name

--- src/AddressBook/AddressBookMain.java
+++ src/AddressBook/AddressBookMain.java
@@ -1,8 +1,42 @@
 package AddressBook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookMain {
 
+    static ArrayList<Contact> addressBook = new ArrayList<>();
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Address Book Program");
+
+        Scanner sc = new Scanner(System.in);
+
+        addressBook.add(new Contact(
+                "Sam", "Arjani", "MG Road",
+                "Bangalore", "KA", "560001",
+                "9999999999", "sam@gmail.com"
+        ));
+
+        System.out.println("Enter First Name to edit:");
+        String name = sc.nextLine();
+
+        for (Contact c : addressBook) {
+            if (c.firstName.equals(name)) {
+
+                System.out.println("Enter New City:");
+                c.city = sc.nextLine();
+
+                System.out.println("Enter New Phone Number:");
+                c.phoneNumber = sc.nextLine();
+
+                System.out.println("Contact updated successfully");
+            }
+        }
+
+        for (Contact c : addressBook) {
+            c.display();
+        }
     }
 }
