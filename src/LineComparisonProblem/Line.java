712d76b00bb621a505cd2266beeb75c6b32b695d
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-16 13:06:27 +0530

    UC3: Compare multiple lines using equals and compareTo

--- src/LineComparisonProblem/Line.java
+++ src/LineComparisonProblem/Line.java
@@ -1,11 +1,39 @@
 package LineComparisonProblem;
 
-public class Line {
+public class Line implements Comparable<Line> {
+
     Point start;
     Point end;
+    Double length;
 
     public Line(Point start, Point end) {
         this.start = start;
         this.end = end;
+        this.length = calculateLength();
+    }
+
+    private double calculateLength() {
+        return Math.sqrt(
+                Math.pow(end.x - start.x, 2) +
+                        Math.pow(end.y - start.y, 2)
+        );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Line otherLine = (Line) obj;
+        return this.length.equals(otherLine.length);
+    }
+
+    @Override
+    public int compareTo(Line other) {
+        return this.length.compareTo(other.length);
     }
 }
--- src/LineComparisonProblem/Main.java
+++ src/LineComparisonProblem/Main.java
@@ -1,7 +1,48 @@
 package LineComparisonProblem;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        Line line1 = new Line(
+                new Point(0, 0),
+                new Point(3, 4)
+        );
+
+        Line line2 = new Line(
+                new Point(1, 1),
+                new Point(4, 5)
+        );
+
+        Line line3 = new Line(
+                new Point(0, 0),
+                new Point(6, 8)
+        );
+
+        System.out.println("Line1 Length: " + line1.length);
+        System.out.println("Line2 Length: " + line2.length);
+        System.out.println("Line3 Length: " + line3.length);
+
+        System.out.println("\n--- Equality Check ---");
+        System.out.println("Line1 equals Line2: " + line1.equals(line2));
+        System.out.println("Line1 equals Line3: " + line1.equals(line3));
+
+        System.out.println("\n--- Comparison ---");
+        compareLines(line1, line2);
+        compareLines(line1, line3);
+        compareLines(line2, line3);
+    }
+
+    private static void compareLines(Line l1, Line l2) {
+        int result = l1.compareTo(l2);
+
+        if (result == 0)
+            System.out.println("Both lines are equal");
+        else if (result > 0)
+            System.out.println("First line is greater than second line");
+        else
+            System.out.println("First line is smaller than second line");
     }
 }
