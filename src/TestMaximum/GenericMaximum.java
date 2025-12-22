0bcbe0ff37f599bbb5d407e3666900f17f9654f4
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:23:31 +0530

    UC2: Find maximum among 3 Floats with test cases

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,18 @@
 package TestMaximum;
 
 public class GenericMaximum {
+
+    public static Integer testMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
+        return max;
+    }
+
+    public static Float testMaximum(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
+        return max;
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,15 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+    public static void main(String[] args) {
+        System.out.println("Float Max First: "
+                + GenericMaximum.testMaximum(3.3f, 2.2f, 1.1f));
+
+        System.out.println("Float Max Second: "
+                + GenericMaximum.testMaximum(1.1f, 3.3f, 2.2f));
+
+        System.out.println("Float Max Third: "
+                + GenericMaximum.testMaximum(1.1f, 2.2f, 3.3f));
+
+    }
 }
