0725cc3c6b3c11488e1f13b0d5febd463e49aeb9
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:18:21 +0530

    UC1: Find maximum among 3 Integers using compareTo with test cases

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,16 @@
 package TestMaximum;
 
 public class GenericMaximum {
+
+    public static Integer testMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+
+        if (b.compareTo(max) > 0)
+            max = b;
+
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,16 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("TC 1.1 Max at First Position: "
+                + GenericMaximum.testMaximum(30, 20, 10));
+
+        System.out.println("TC 1.2 Max at Second Position: "
+                + GenericMaximum.testMaximum(10, 30, 20));
+
+        System.out.println("TC 1.3 Max at Third Position: "
+                + GenericMaximum.testMaximum(10, 20, 30));
+    }
 }
