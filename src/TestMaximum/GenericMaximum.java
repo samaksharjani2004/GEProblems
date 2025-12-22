47c04f7b0024c92b4755cd526327253bb662da55
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:28:25 +0530

    Refactor2: Generic class with constructor and instance variables

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,29 @@
 package TestMaximum;
 
-public class GenericMaximum {
+public class GenericMaximum<T extends Comparable<T>> {
+
+    T a, b, c;
+
+    public GenericMaximum(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T testMaximum() {
+        return testMaximum(a, b, c);
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+
+        T max = a;
+
+        if (b.compareTo(max) > 0)
+            max = b;
+
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,19 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+
+    public static void main(String[] args) {
+
+        GenericMaximum<Integer> intTest =
+                new GenericMaximum<>(10, 40, 30);
+
+        System.out.println("Generic Class Integer Max: "
+                + intTest.testMaximum());
+
+        GenericMaximum<String> stringTest =
+                new GenericMaximum<>("Apple", "Peach", "Banana");
+
+        System.out.println("Generic Class String Max: "
+                + stringTest.testMaximum());
+    }
 }
