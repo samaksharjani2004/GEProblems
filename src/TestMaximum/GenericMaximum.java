969bcaf9ed0bbcddea76ff0099e3713b177ae757
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:29:49 +0530

    UC4: Extend maximum method to support more than three values

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,11 @@
 package TestMaximum;
 
-public class GenericMaximum {
+import java.util.Arrays;
+
+public class GenericMaximum<T extends Comparable<T>> {
+
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        Arrays.sort(values);
+        return values[values.length - 1];
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,14 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Max of multiple Integers: "
+                + GenericMaximum.testMaximum(10, 20, 30, 40));
+
+        System.out.println("Max of multiple Strings: "
+                + GenericMaximum.testMaximum("Apple", "Peach", "Banana", "Mango"));
+    }
 }
+
