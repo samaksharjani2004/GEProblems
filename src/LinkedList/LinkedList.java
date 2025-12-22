54f08951024058c4fb90578b44ca941ea489f40e
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 15:46:49 +0530

    Linked List Program

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,4 +1,141 @@
 package LinkedList;
 
 public class LinkedList {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    /* UC-1 / UC-2 : Add at head */
+    public void add(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    /* UC-3 : Append at end */
+    public void append(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+
+        temp.next = newNode;
+    }
+
+    /* UC-4 & UC-8 : Insert after a key */
+    public void insertAfter(int key, int data) {
+        Node temp = head;
+
+        while (temp != null && temp.data != key)
+            temp = temp.next;
+
+        if (temp != null) {
+            Node newNode = new Node(data);
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    /* UC-5 : Delete first */
+    public void pop() {
+        if (head != null)
+            head = head.next;
+    }
+
+    /* UC-6 : Delete last */
+    public void popLast() {
+        if (head == null || head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next.next != null)
+            temp = temp.next;
+
+        temp.next = null;
+    }
+
+    /* UC-7 : Search */
+    public boolean search(int key) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    /* UC-9 : Delete by key */
+    public void delete(int key) {
+        if (head == null)
+            return;
+
+        if (head.data == key) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data != key)
+            temp = temp.next;
+
+        if (temp.next != null)
+            temp.next = temp.next.next;
+    }
+
+    /* UC-9 : Size */
+    public int size() {
+        int count = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    /* UC-10 : Sorted Insert */
+    public void addSorted(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null || head.data >= data) {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null && temp.next.data < data)
+            temp = temp.next;
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    /* Display */
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
--- src/LinkedList/LinkedListTest.java
+++ src/LinkedList/LinkedListTest.java
@@ -1,4 +1,64 @@
 package LinkedList;
 
 public class LinkedListTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Linked List Program");
+
+        LinkedList list = new LinkedList();
+
+        // UC-1 / UC-2
+        list.add(70);
+        list.add(30);
+        list.add(56);
+        list.display();
+
+        // UC-3
+        LinkedList list2 = new LinkedList();
+        list2.append(56);
+        list2.append(30);
+        list2.append(70);
+        list2.display();
+
+        // UC-4
+        LinkedList list3 = new LinkedList();
+        list3.append(56);
+        list3.append(70);
+        list3.insertAfter(56, 30);
+        list3.display();
+
+        // UC-5
+        list3.pop();
+        list3.display();
+
+        // UC-6
+        LinkedList list4 = new LinkedList();
+        list4.append(56);
+        list4.append(30);
+        list4.append(70);
+        list4.popLast();
+        list4.display();
+
+        // UC-7
+        System.out.println("Search 30: " + list4.search(30));
+
+        // UC-8
+        list4.insertAfter(30, 40);
+        list4.display();
+
+        // UC-9
+        list4.delete(40);
+        list4.display();
+        System.out.println("Size: " + list4.size());
+
+        // UC-10
+        LinkedList sortedList = new LinkedList();
+        sortedList.addSorted(56);
+        sortedList.addSorted(30);
+        sortedList.addSorted(40);
+        sortedList.addSorted(70);
+        sortedList.display();
+    }
 }
+
