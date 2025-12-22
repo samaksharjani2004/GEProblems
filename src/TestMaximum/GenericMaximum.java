9720fe1ca964805bcae8c00b8e9a7c805206bf74
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:27:32 +0530

    Refactor1: Generic method using Comparable

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,17 @@
 package TestMaximum;
 
 public class GenericMaximum {
+
+    public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+
+        T max = a;
+
+        if (b.compareTo(max) > 0)
+            max = b;
+
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,16 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Integer Max: "
+                + GenericMaximum.testMaximum(10, 20, 30));
+
+        System.out.println("Float Max: "
+                + GenericMaximum.testMaximum(3.3f, 2.2f, 1.1f));
+
+        System.out.println("String Max: "
+                + GenericMaximum.testMaximum("Apple", "Peach", "Banana"));
+    }
 }
