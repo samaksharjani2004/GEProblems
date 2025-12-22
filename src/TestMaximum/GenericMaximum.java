c87468eced4ae74b2554404aeab66e722f1d0bbf
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:26:20 +0530

    UC3: Find maximum among 3 Strings using compareTo

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,16 @@
 package TestMaximum;
 
 public class GenericMaximum {
+
+    public static String testMaximum(String a, String b, String c) {
+        String max = a;
+
+        if (b.compareTo(max) > 0)
+            max = b;
+
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        return max;
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,12 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Test Maximum Program");
+
+        System.out.println("String Max: "
+                + GenericMaximum.testMaximum("Apple", "Peach", "Banana"));
+    }
 }
