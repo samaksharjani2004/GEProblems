82973b165a99ba8b48820ca48ff122cc84630205
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-12-22 14:30:50 +0530

    UC5: Print maximum value using generic method

--- src/TestMaximum/GenericMaximum.java
+++ src/TestMaximum/GenericMaximum.java
@@ -1,4 +1,14 @@
 package TestMaximum;
 
-public class GenericMaximum {
+public class GenericMaximum<T extends Comparable<T>> {
+
+    public static <T extends Comparable<T>> void printMax(T... values) {
+        T max = testMaximum(values);
+        System.out.println("Maximum value is: " + max);
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        java.util.Arrays.sort(values);
+        return values[values.length - 1];
+    }
 }
--- src/TestMaximum/GenericMaximumTest.java
+++ src/TestMaximum/GenericMaximumTest.java
@@ -1,4 +1,10 @@
 package TestMaximum;
 
 public class GenericMaximumTest {
+
+    public static void main(String[] args) {
+
+        GenericMaximum.printMax(10, 40, 30, 20);
+        GenericMaximum.printMax("Apple", "Peach", "Banana", "Mango");
+    }
 }
